df8783d40227f79c3379bda36e5bd478950e299c
wangcheng1 <wangcheng1@bestseller.com.cn>
2022-02-17 19:05:51 +0800

    login page

---
+++ lib/models/changeLang.dart
@@ -0,0 +1,13 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/foundation.dart';
+
+class changeLangModel extends ChangeNotifier {
+  bool isEn = false;
+  changeLang(val) {
+    isEn = !val;
+    notifyListeners();
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,36 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+/// A simple widget that builds different things on different platforms.
+class PlatformWidget extends StatelessWidget {
+  const PlatformWidget({
+    Key? key,
+    required this.androidBuilder,
+    required this.iosBuilder,
+  }) : super(key: key);
+
+  final WidgetBuilder androidBuilder;
+  final WidgetBuilder iosBuilder;
+
+  @override
+  Widget build(context) {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return androidBuilder(context);
+      case TargetPlatform.iOS:
+        return iosBuilder(context);
+      default:
+        assert(false, 'Unexpected platform $defaultTargetPlatform');
+        return const SizedBox.shrink();
+    }
+  }
+}
+
+
+
+
